84a89df2b4d51b85cf8075d056f388386725a3f4
Sam Ronning <sronning@umich.edu>
2022-02-12 13:12:11 -0500

    default local url

--- src/services/pyServer.js
+++ src/services/pyServer.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 const { REACT_APP_PYSERVER_URL } = process.env;
-const pyClient = axios.create({ baseURL: REACT_APP_PYSERVER_URL });
+const pyClient = axios.create({
+  baseURL: REACT_APP_PYSERVER_URL || "http://localhost:5000",
+});
 
 export { pyClient };
