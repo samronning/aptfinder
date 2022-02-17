73195db10dac91752b305366afbc347d24f6247b
Sam Ronning <sronning@umich.edu>
2022-02-16 23:27:06 -0500

    fix height for all devices

--- src/components/NyAnimator.js
+++ src/components/NyAnimator.js
@@ -3,27 +3,34 @@ import { animated, useSpring, config } from "react-spring";
 
 const NyAnimator = ({ followMouse, leave, loader }) => {
   const loaderSpring = useSpring({
-    from: { opacity: 0, scale: 0.3, top: -400, rotateZ: 0 },
+    from: {
+      opacity: 0,
+      rotateZ: 0,
+      transform: "scale(0.3) translateY(68%)",
+    },
     to: async (next) => {
       await next({ opacity: 1 });
       await next({
-        rotateZ: 360,
         loop: true,
+        rotateZ: 360,
+        transform: "scale(0.3) translateY(68%)",
         config: { ...config.wobbly, tension: 100 },
       });
     },
   });
   return (
     <animated.svg
-      width="15in"
-      height="15in"
+      width="100vw"
+      height="100vw"
       version="1.1"
-      viewBox="0 0 131.33 131.33"
+      viewBox="0 0 132 132"
       xmlns="http://www.w3.org/2000/svg"
       style={{
+        transform: !loader && "translateY(51%)",
         ...(loader && loaderSpring),
         width: "100%",
         position: "fixed",
+        bottom: 0,
         overflow: "overlay",
         pointerEvents: "none",
       }}
