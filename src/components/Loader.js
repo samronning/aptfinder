19ca0f1e8f15d6faf4009e10561d7b1ed68c93b7
Sam Ronning <sronning@umich.edu>
2022-02-19 11:12:49 -0500

    improve placement of animation

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,22 @@
 import { useEffect } from "react";
 import NyAnimator from "./NyAnimator";
-const Loader = ({ active, nullVal, setResource, resourceGetter }) => {
+const Loader = ({
+  active = false,
+  nullVal = null,
+  setResource,
+  resourceGetter,
+}) => {
   useEffect(() => {
-    resourceGetter()
-      .then((res) => {
-        setResource(res);
-      })
-      .catch(() => {
-        setResource(nullVal);
-      });
-  }, [setResource, resourceGetter]);
+    if (resourceGetter && setResource) {
+      resourceGetter()
+        .then((res) => {
+          setResource(res);
+        })
+        .catch(() => {
+          setResource(nullVal);
+        });
+    }
+  }, [nullVal, setResource, resourceGetter]);
   return active && <NyAnimator loader={true} />;
 };
 
--- src/components/NyAnimator.js
+++ src/components/NyAnimator.js
@@ -6,43 +6,50 @@ const NyAnimator = ({ followMouse, leave, loader }) => {
     from: {
       opacity: 0,
       rotateZ: 0,
-      transform: "scale(0.3) translateY(68%)",
+      scale: 0.3,
     },
     to: async (next) => {
       await next({ opacity: 1 });
       await next({
         loop: true,
         rotateZ: 360,
-        transform: "scale(0.3) translateY(68%)",
-        config: { ...config.wobbly, tension: 100 },
+        config: { ...config.wobbly, tension: 60 },
+        delay: 500,
       });
     },
   });
   return (
-    <animated.svg
-      width="100vw"
-      height="100vw"
-      version="1.1"
-      viewBox="0 0 132 132"
-      xmlns="http://www.w3.org/2000/svg"
+    <div
       style={{
-        transform: !loader && "translateY(51%)",
-        ...(loader && loaderSpring),
-        width: "100%",
-        position: "fixed",
-        bottom: 0,
-        overflow: "overlay",
-        pointerEvents: "none",
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
       }}
     >
-      {
-        <Skylines
-          followMouse={followMouse}
-          leave={leave}
-          disableTrans={loader}
-        />
-      }
-    </animated.svg>
+      <animated.svg
+        width="100%"
+        version="1.1"
+        viewBox={loader ? "0 0 132 132" : "0 -66 132 132"}
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          ...(loader && loaderSpring),
+          position: loader ? "static" : "fixed",
+          bottom: 0,
+          overflow: "overlay",
+          pointerEvents: "none",
+        }}
+      >
+        {
+          <Skylines
+            followMouse={followMouse}
+            leave={leave}
+            disableTrans={loader}
+          />
+        }
+      </animated.svg>
+    </div>
   );
 };
 
